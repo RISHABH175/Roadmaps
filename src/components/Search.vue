f4b6412bfad61e744805af427dd4630cc71b0d81
Rishabh Shyam Sunder <60140002+RISHABH175@users.noreply.github.com>
2022-01-10 20:04:01 +0530

    Updated Search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="search-main">
+    <h1 style="color:white">Enter the Domain</h1>
     <input
       id="requestChart"
       type="text"
@@ -40,7 +41,7 @@ export default {
             for (var i = 0; i < len; i++) {
               document.getElementById(
                 "searchRes"
-              ).innerHTML += `<div id="card${i}" class='blocks'><p>Name: ${resp.data[i].mapN}</p><p>Tag: ${resp.data[i].mapT}</p><button ><a href="http://localhost:8080/View?id=${i}&mapN=${resp.data[i].mapN}">Click!</a></button></div>`;
+              ).innerHTML += `<div id="card${i}" class='blocks'><p style="  color:white;">Name: ${resp.data[i].mapN}</p><p style="  color:white;">Author Name: ${resp.data[i].mapA}</p><p style="  color:white;">Tag: ${resp.data[i].mapT}</p><button><a href="http://localhost:8080/View?id=${i}&mapN=${resp.data[i].mapN}">Click!</a></button></div>`;
               //document.getElementById(`card${i}`).addEventListener("click",this.selectLog(i));
             }
           }
@@ -52,15 +53,21 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@900&display=swap');
 .search-main {
-  width: 100%;
+ width: 50%;
   height: 100%;
   background-color: black;
+
+  margin: auto;
 }
 
 button {
+  margin-left:20px;
+  margin-top:20px;
   padding: 9px 25px;
   background-color: rgb(103, 64, 245);
+  color: white;
   border: none;
   border-radius: 50px;
   cursor: pointer;
@@ -72,25 +79,45 @@ button {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  color: white;
+  color:white;
+
   gap: 2rem;
   margin-top: 100px;
 }
 
 .blocks {
-  display: flex;
+   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
   width: 250px;
   height: 250px;
   border-width: 2px;
-  border-color: antiquewhite;
+  color:white;
+   
+  
+  border-color: purple;
   border-radius: 15px;
   border-style: solid;
   padding: 10px;
 }
 
 a{
+  
+  color:white;
+  text-decoration:none;
+}
+a:hover{
+  font-size: 20px;
+  color:white;
+  text-decoration:none;
+}
+
+#requestChart
+{
   color: black;
+}
+h1{
+  font-family: 'Cinzel Decorative', cursive;
+  font-size: 80px;
 }
 </style>
