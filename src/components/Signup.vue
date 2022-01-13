b5f6a5e6d4ca41eda9ea4177f7df6d6fb298007d
Rishabh Shyam Sunder <60140002+RISHABH175@users.noreply.github.com>
2022-01-13 20:40:11 +0530

    updated Sign up

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -5,20 +5,20 @@
      </div>
     <div class="inner-block" >
     <form v-on:submit.prevent="createUser">
-      <h3>Sign Up</h3>
+      <h3 style="color:black">Sign Up</h3>
 
       <div class="form-group">
-        <label>Full Name</label>
+        <label style="color:black">Full Name</label>
         <input type="text" id="fullName" class="form-control form-control-lg" v-model="form.name"/>
       </div>
 
       <div class="form-group">
-        <label>Email address</label>
+        <label style="color:black">Email address</label>
         <input type="email" id="email" class="form-control form-control-lg" v-model="form.email" />
       </div>
 
       <div class="form-group">
-        <label>Password</label>
+        <label style="color:black">Password</label>
         <input
           type="password"
           id="password"
@@ -27,13 +27,13 @@
         />
       </div>
 
-      <button type="submit" class="btn btn-dark btn-lg btn-block">
+      <button type="submit" class="btn btn-dark btn-lg btn-block" >
         Sign Up
       </button>
 
-      <p class="forgot-password text-right">
+      <p class="forgot-password text-right" style="color:black">
         Already registered
-        <router-link :to="{ name: 'login' }">sign in?</router-link>
+        <router-link :to="{ name: 'login' }" style="color:blue">sign in?</router-link>
       </p>
     </form>
     </div>
